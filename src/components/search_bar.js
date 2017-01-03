70479e355f99e155b8d8a8cbba2ce18a2720bb89
Tjitske Lohstroh <tjitske@lohstroh.nl>
2017-01-03 12:51:49 +0100

    Add Search debounce

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -19,18 +19,19 @@ class SearchBar extends Component{
   render() {
     // return <input onChange={this.onInputChange} />
     return (
-      <div>
+      <div className="search_bar">
         <input
         value ={this.state.term}
-        onChange={ event => this.setState({term: event.target.value})} />
-        {/* Value of the input: { this.state.term } */}
+        onChange={ event => this.onInputChange(event.target.value)} />
       </div>
     )
   }
 
-  // onInputChange(event) {
-  //   console.log(event.target.value);
-  // }
+  onInputChange(term){
+    this.setState({term});
+    this.props.onSearchTermChange(term);
+  }
+
 }
 
 export default SearchBar;
