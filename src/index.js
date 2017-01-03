5a463a5c8de6e6a301c969389d27217fbc91bad5
Tjitske Lohstroh <tjitske@lohstroh.nl>
2017-01-03 12:05:36 +0100

    Add Video selecting

--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ import YTSearch from 'youtube-api-search'
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
 import VideoDetail from './components/video_detail';
-const API_KEY = 'AIzaSyChJa4X6mWN71-mqiShCxXRgdHc03B3Huw';
+
 // const API_KEY = process.env.YOUTUBE_API;
+
 // YTSearch({key:API_KEY, term: 'surfboards'}, function(data) {
 //   console.log(data);
 // });
@@ -17,11 +18,16 @@ class App extends Component {
   constructor(props){
     super(props);
 
-    this.state = { videos: [] };
+    this.state = {
+      videos: [],
+      selectedVideo: null
+     };
 
     YTSearch({key:API_KEY, term: 'surfboards'}, (videos) => {
-      this.setState({ videos });
-      // this.setState({ videos: videos });
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
     });
   }
 
@@ -29,8 +35,10 @@ class App extends Component {
     return(
       <div>
         <SearchBar />
-        <VideoDetail video={this.state.videos[0]} />
-        <VideoList videos={this.state.videos} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
+          videos={this.state.videos} />
       </div>
     );
   }
